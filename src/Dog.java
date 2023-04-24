9ef2bab2d3fdaa8044a18a1ea3ad2f76306b9eba
grifferthrydwy <78763622+grifferthrydwy@users.noreply.github.com>
2023-04-24 09:29:51 -0400

    added a few variables and a basic constructor to the Dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,10 @@
+import javafx.scene.paint.Color;
+
 public class Dog {
+
+    int variant;
+    Color color;
+    public Dog(int hueInput){
+        this.color = Color.hsb(hueInput, 1, 1);
+    }
 }
