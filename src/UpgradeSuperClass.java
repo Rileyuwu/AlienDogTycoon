ff22ab0caa291dbb767c0f6408a947652d352bce
grifferthrydwy <78763622+grifferthrydwy@users.noreply.github.com>
2023-04-28 09:13:56 -0400

    test commit

--- src/UpgradeSuperClass.java
+++ src/UpgradeSuperClass.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
+
 public class UpgradeSuperClass {
 
     //Constants for upgrade costs
@@ -9,24 +10,23 @@ public class UpgradeSuperClass {
 
     //Variables for player's money and upgrade status
     int playerMoney;
-    boolean groomingMachineUpgraded;
-    boolean foodFactoryUpgraded;
-    boolean growthChamberUpgraded;
+    int groomingMachineTier;
+    int foodFactoryTier;
+    int growthChamberTier;
 
     public UpgradeSuperClass() {
 
         playerMoney = 1000;
-        groomingMachineUpgraded = false;
-        foodFactoryUpgraded = false;
-        growthChamberUpgraded = false;
+        groomingMachineTier = 1;
+        foodFactoryTier = 1;
+        growthChamberTier = 1;
 
     }
 
-
-    enum Rarity{
-        /**
-         * Enum that defines each of the rarities
-         */
+    /**
+     * enum that contains dog rarity
+     */
+    enum Rarity {
         COMMON,
         UNCOMMON,
         RARE,
@@ -34,45 +34,47 @@ public class UpgradeSuperClass {
         LEGENDARY,
         MYTHIC
     }
-    public Rarity getNewDogRarity(int tier){
-        /** @param tier the upgrade tier of the growth chamber
-         *  @author Edwin Fleming
-         *  @version 4-26-2023
-         *  determines the rarity of newly grown dogs based on growth chamber tier
-         */
-        int random = (int) (Math.random()*6);
+
+    /**
+     * this function handles the rarity of different rarities of dog for different growth chamber tiers
+     *
+     * @param tier the tier of growth chamber the player has unlocked
+     * @return the rarity of the newly generated dog
+     */
+    public Rarity getNewDogRarity(int tier) {
+        int random = (int) (Math.random() * 6);
         switch (tier) {
             case 1:
-                if (random == 1|random == 2|random == 3){
+                if (random == 1 | random == 2 | random == 3) {
                     return Rarity.COMMON;
-                } else if (random == 4|random == 5){
+                } else if (random == 4 | random == 5) {
                     return Rarity.UNCOMMON;
                 } else if (random == 6) {
                     return Rarity.RARE;
                 }
                 break;
             case 2:
-                if (random == 1|random == 2|random == 3){
+                if (random == 1 | random == 2 | random == 3) {
                     return Rarity.UNCOMMON;
-                } else if (random == 4|random == 5){
+                } else if (random == 4 | random == 5) {
                     return Rarity.RARE;
                 } else if (random == 6) {
                     return Rarity.EPIC;
                 }
                 break;
             case 3:
-                if (random == 1|random == 2|random == 3){
+                if (random == 1 | random == 2 | random == 3) {
                     return Rarity.RARE;
-                } else if (random == 4|random == 5){
+                } else if (random == 4 | random == 5) {
                     return Rarity.EPIC;
                 } else if (random == 6) {
                     return Rarity.LEGENDARY;
                 }
                 break;
             case 4:
-                if (random == 1|random == 2|random == 3){
+                if (random == 1 | random == 2 | random == 3) {
                     return Rarity.EPIC;
-                } else if (random == 4|random == 5){
+                } else if (random == 4 | random == 5) {
                     return Rarity.LEGENDARY;
                 } else if (random == 6) {
                     return Rarity.MYTHIC;
@@ -82,7 +84,7 @@ public class UpgradeSuperClass {
         }
     }
 
-    public void startUpgrade(){
+    public void startUpgrade() {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -99,19 +101,20 @@ public class UpgradeSuperClass {
             System.out.println("3. Upgrade Breeding System ($" + GROWTH_CHAMBER_COST + ")");
             System.out.println("4. Exit");
 
-    }
-
-         void upgradeGroomingMachine() {
-            if (playerMoney >= GROOMING_MACHINE_COST && !groomingMachineUpgraded) {
-                playerMoney -= GROOMING_MACHINE_COST;
-                groomingMachineUpgraded = true;
-                System.out.println("Grooming Machine upgraded!");
-                System.out.println("New grooming machine functions unlocked.");
-            } else if (groomingMachineUpgraded) {
-                System.out.println("Your grooming machine is already upgraded!");
-            } else {
-                System.out.println("You don't have enough money to upgrade your grooming machine!");
-            }
         }
     }
+
+    void upgradeGroomingMachine() {
+        if (playerMoney >= GROOMING_MACHINE_COST && !groomingMachineUpgraded) {
+            playerMoney -= GROOMING_MACHINE_COST;
+            groomingMachineUpgraded = true;
+            System.out.println("Grooming Machine upgraded!");
+            System.out.println("New grooming machine functions unlocked.");
+        } else if (groomingMachineUpgraded) {
+            System.out.println("Your grooming machine is already upgraded!");
+        } else {
+            System.out.println("You don't have enough money to upgrade your grooming machine!");
+        }
+    }
+
 }
