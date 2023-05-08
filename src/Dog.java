11e151a0ee52b868992b5e4be2415456742a9de0
grifferthrydwy <78763622+grifferthrydwy@users.noreply.github.com>
2023-05-08 12:59:32 -0400

    lots of minor changes

--- src/Dog.java
+++ src/Dog.java
@@ -1,15 +1,24 @@
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Dog {
 
-    int variant;
+    String variant;
     UpgradeSuperClass.Rarity rarity;
     Color color;
     public Dog(int hueInput){
         this.color = Color.hsb(hueInput, 1, 1);
     }
 
-    void drawDog(){
-        
+
+    public void setVariant(String variant){
+        this.variant = variant;
+    }
+
+    public String getVariant(){
+        return this.variant;
+    }
+    void drawDog(GraphicsContext pen){
+
     }
 }
